daefa846dc4661285ed90a45b295a95a8e80fe32
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-15 14:02:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+
+let input = document.querySelector('#evaluatedText');
+let out = document.querySelector('#letterCount');
+
+
+input.addEventListener('keyup', (e)=>{
+  out.innerHTML = '';
+  out.innerHTML = e.target.value.length;
+})
